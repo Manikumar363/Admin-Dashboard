243c360347307032f4add1bf9790284b52dcda3d
Manikumar <134723364+Manikumar363@users.noreply.github.com>
2025-06-04 13:28:00 +0530

    Update authService.js

--- frontend/src/services/authService.js
+++ frontend/src/services/authService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'https://admin-dashboard-15rx.onrender.com';
+const API_URL = 'https://admin-dashboard-15rx.onrender.com/api/auth';
 
 // Create axios instance
 const api = axios.create({
